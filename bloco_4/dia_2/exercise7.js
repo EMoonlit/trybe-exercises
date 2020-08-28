f3bbc171e7eff567147babb1249217fd49d0ee55
EmersonNJ <emoonlit@pop-os.localdomain>
2020-08-28 19:28:29 -0300

    Add Infinity in Let, exercise7

--- bloco_4/dia_2/exercise7.js
+++ bloco_4/dia_2/exercise7.js
@@ -4,7 +4,7 @@
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-let lowerNumber = 1000;
+let lowerNumber = Infinity;
 
 for ( i = 0; i < numbers.length; i += 1) {
     if (numbers[i] < lowerNumber) {
