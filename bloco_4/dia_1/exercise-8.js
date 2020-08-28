5abf384b781ba47bc631625772ae8a6a58574f59
EmersonNJ <emoonlit@pop-os.localdomain>
2020-08-28 13:35:58 -0300

    Exercises JavaScript-not complete.

---
+++ bloco_4/dia_1/exercise-8.js
@@ -0,0 +1,10 @@
+let a = 1;
+let b = 3;
+let c = 5;
+
+let isEven = false;
+
+if ( a % == 0 || b % == 0 || c % == 0) {
+    isEven = true;
+}
+console.log(isEven);
---
+++ bloco_4/dia_1/exercise-9.js
@@ -0,0 +1,12 @@
+let a = 2;
+let b = 2;
+let c = 4;
+
+let isOdd;
+
+if (a % 2 != 0 || b % 2 != 0 || c % 2 != 0) {
+    isOdd = true;
+} else {
+    isOdd = false;
+}
+console.log(isOdd)
